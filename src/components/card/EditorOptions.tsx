52c5d71f280cf244a60bfcfc27ad28e2bd2738dd
Harshit Raj <harshitr20@iitk.ac.in>
2023-10-09 17:01:38 +0530

    Add editor options

---
+++ src/components/card/EditorOptions.tsx
@@ -0,0 +1,77 @@
+// Chakra imports
+import {
+	Flex,
+	Stat,
+	StatLabel,
+	StatNumber,
+	useColorModeValue,
+	Icon,
+	SimpleGrid,
+} from "@chakra-ui/react";
+// Custom components
+import Card from "components/card/Card";
+import BoldButton from "components/button/BoldButton";
+import ItalicButton from "components/button/ItalicButton";
+import UnderlineButton from "components/button/UnderlineButton";
+import IconBox from "components/icons/IconBox";
+import { MdEditDocument } from "react-icons/md";
+
+export default function Default(props: { name: string }) {
+	const { name } = props;
+	const textColor = useColorModeValue("secondaryGray.900", "white");
+	const textColorSecondary = "secondaryGray.600";
+	// Chakra Color Mode
+	const brandColor = useColorModeValue("brand.500", "white");
+	const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+
+	return (
+		<Card py="15px">
+			<Flex
+				my="auto"
+				h="100%"
+				align={{ base: "center", xl: "start" }}
+				justify={{ base: "center", xl: "center" }}
+			>
+				<IconBox
+					w="56px"
+					h="56px"
+					bg={boxBg}
+					icon={
+						<Icon w="32px" h="32px" as={MdEditDocument} color={brandColor} />
+					}
+				/>
+
+				<Stat my="auto" ms="18px">
+					<StatLabel
+						lineHeight="100%"
+						color={textColorSecondary}
+						fontSize={{
+							base: "sm",
+						}}
+					>
+						{name}
+					</StatLabel>
+					<StatNumber
+						color={textColor}
+						fontSize={{
+							base: "xl",
+						}}
+					>
+						<SimpleGrid
+							columns={{ base: 4, md: 8, lg: 12, "2xl": 12 }}
+							gap="20px"
+							mt="10px"
+						>
+							<BoldButton />
+							<ItalicButton />
+							<UnderlineButton />
+						</SimpleGrid>
+					</StatNumber>
+				</Stat>
+				<Flex ms="auto" w="max-content">
+					{}
+				</Flex>
+			</Flex>
+		</Card>
+	);
+}
--- src/views/editor/default/index.tsx
+++ src/views/editor/default/index.tsx
@@ -21,21 +21,14 @@
 */
 
 // Chakra imports
-import { Avatar, Box, Flex, FormLabel, Icon, Select, SimpleGrid, useColorModeValue } from '@chakra-ui/react';
+import { Box, Icon, SimpleGrid, useColorModeValue } from '@chakra-ui/react';
 // Assets
-import Usa from 'assets/img/dashboards/usa.png';
 // Custom components
-import MiniCalendar from 'components/calendar/MiniCalendar';
-import MiniStatistics from 'components/card/MiniStatistics';
+import MiniStatistics from "components/card/MiniStatistics";
+import EditorOptions from "components/card/EditorOptions";
 import IconBox from 'components/icons/IconBox';
-import { MdAddTask, MdAttachMoney, MdBarChart, MdFileCopy } from 'react-icons/md';
+import { MdAddTask, MdFileCopy } from 'react-icons/md';
 import EditorArea from 'views/editor/default/components/EditorArea';
-import DailyTraffic from 'views/editor/default/components/DailyTraffic';
-import PieCard from 'views/editor/default/components/PieCard';
-import Tasks from 'views/editor/default/components/Tasks';
-import TotalSpent from 'views/editor/default/components/TotalSpent';
-import WeeklyRevenue from 'views/editor/default/components/WeeklyRevenue';
-import tableDataComplex from 'views/editor/default/variables/tableDataComplex';
 
 export default function UserReports() {
 	// Chakra Color Mode
@@ -43,48 +36,13 @@ export default function UserReports() {
 	const boxBg = useColorModeValue('secondaryGray.300', 'whiteAlpha.100');
 	return (
 		<Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
-			<SimpleGrid columns={{ base: 1, md: 2, lg: 3, '2xl': 6 }} gap='20px' mb='20px'>
-				<MiniStatistics
-					startContent={
-						<IconBox
-							w='56px'
-							h='56px'
-							bg={boxBg}
-							icon={<Icon w='32px' h='32px' as={MdBarChart} color={brandColor} />}
-						/>
-					}
-					name='Earnings'
-					value='$350.4'
+			<SimpleGrid columns={{ base: 1, md: 2, lg: 2, '2xl': 2 }} gap='20px' mb='20px'>
+				<EditorOptions
+					name='Editor Options'
 				/>
-				<MiniStatistics
-					startContent={
-						<IconBox
-							w='56px'
-							h='56px'
-							bg={boxBg}
-							icon={<Icon w='32px' h='32px' as={MdAttachMoney} color={brandColor} />}
-						/>
-					}
-					name='Spend this month'
-					value='$642.39'
-				/>
+
 				<MiniStatistics growth='+23%' name='Sales' value='$574.34' />
-				<MiniStatistics
-					endContent={
-						<Flex me='-16px' mt='10px'>
-							<FormLabel htmlFor='balance'>
-								<Avatar src={Usa} />
-							</FormLabel>
-							<Select id='balance' variant='mini' mt='5px' me='0px' defaultValue='usd'>
-								<option value='usd'>USD</option>
-								<option value='eur'>EUR</option>
-								<option value='gba'>GBA</option>
-							</Select>
-						</Flex>
-					}
-					name='Your balance'
-					value='$1,000'
-				/>
+
 				<MiniStatistics
 					startContent={
 						<IconBox
