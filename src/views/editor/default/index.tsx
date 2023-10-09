f1114d285ca84b18cc689b733e8c5da522aa378d
Harshit Raj <harshitr20@iitk.ac.in>
2023-10-09 17:45:29 +0530

    Add word count feature

--- src/views/editor/default/index.tsx
+++ src/views/editor/default/index.tsx
@@ -21,56 +21,70 @@
 */
 
 // Chakra imports
-import { Box, Icon, SimpleGrid, useColorModeValue } from '@chakra-ui/react';
+import { Box, Icon, SimpleGrid, useColorModeValue } from "@chakra-ui/react";
 // Assets
 // Custom components
 import MiniStatistics from "components/card/MiniStatistics";
 import EditorOptions from "components/card/EditorOptions";
-import IconBox from 'components/icons/IconBox';
-import { MdAddTask, MdFileCopy } from 'react-icons/md';
-import EditorArea from 'views/editor/default/components/EditorArea';
+import IconBox from "components/icons/IconBox";
+import { MdAddTask, MdFileCopy } from "react-icons/md";
+import EditorArea from "views/editor/default/components/EditorArea";
+import { useState } from "react";
 
 export default function UserReports() {
 	// Chakra Color Mode
-	const brandColor = useColorModeValue('brand.500', 'white');
-	const boxBg = useColorModeValue('secondaryGray.300', 'whiteAlpha.100');
+	const brandColor = useColorModeValue("brand.500", "white");
+	const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+
+	const [wordCount, setWrordCount] = useState(0);
+
+	const handleContentChange = (e: React.SyntheticEvent) => {
+		const target = e.target as HTMLDivElement;
+		console.log(target.innerHTML);
+		setWrordCount(target.innerText.trim().split(/\s+/).length);
+	};
+
 	return (
-		<Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
-			<SimpleGrid columns={{ base: 1, md: 2, lg: 2, '2xl': 2 }} gap='20px' mb='20px'>
-				<EditorOptions
-					name='Editor Options'
-				/>
+		<Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+			<SimpleGrid
+				columns={{ base: 1, md: 2, lg: 2, "2xl": 2 }}
+				gap="20px"
+				mb="20px"
+			>
+				<EditorOptions name="Editor Options" />
 
-				<MiniStatistics growth='+23%' name='Sales' value='$574.34' />
+				<MiniStatistics growth="+23%" name="Sales" value="$574.34" />
 
 				<MiniStatistics
 					startContent={
 						<IconBox
-							w='56px'
-							h='56px'
-							bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
-							icon={<Icon w='28px' h='28px' as={MdAddTask} color='white' />}
+							w="56px"
+							h="56px"
+							bg="linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)"
+							icon={<Icon w="28px" h="28px" as={MdAddTask} color="white" />}
 						/>
 					}
-					name='New Tasks'
-					value='154'
+					name="New Tasks"
+					value="154"
 				/>
 				<MiniStatistics
 					startContent={
 						<IconBox
-							w='56px'
-							h='56px'
+							w="56px"
+							h="56px"
 							bg={boxBg}
-							icon={<Icon w='32px' h='32px' as={MdFileCopy} color={brandColor} />}
+							icon={
+								<Icon w="32px" h="32px" as={MdFileCopy} color={brandColor} />
+							}
 						/>
 					}
-					name='Total Words'
-					value='2935'
+					name="Total Words"
+					value={wordCount}
 				/>
 			</SimpleGrid>
 
-			<SimpleGrid columns={{ base: 1, md: 1, xl: 1 }} gap='20px' mb='20px'>
-				<EditorArea />
+			<SimpleGrid columns={{ base: 1, md: 1, xl: 1 }} gap="20px" mb="20px">
+				<EditorArea handleContentChange={handleContentChange} />
 			</SimpleGrid>
 		</Box>
 	);
