a82e90344f35d70be4f29fcc5eec8da5e079310e
Abhimanyu Sethia <51320930+abhimanyusethia12@users.noreply.github.com>
2023-10-10 17:21:32 +0530

    remove Firefox compatibility issues in BoldButton
    Co-authored-by: Harshit Raj <75195728+1-Harshit@users.noreply.github.com>

--- src/components/button/JustifyCenterButton.tsx
+++ src/components/button/JustifyCenterButton.tsx
@@ -7,8 +7,14 @@ export default function Default() {
 	const textColor = useColorModeValue("secondaryGray.900", "white");
 	const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
 
-	const handleJustifyCenterClick = () => {
-		document.execCommand("justifyCenter", true, "");
+	const handleAlignCenterClick = () => {
+		if (document.queryCommandSupported("justifyCenter")) {
+			document.execCommand("justifyCenter", false, null);
+		} else {
+			document.execCommand("styleWithCSS", false, "true");
+			document.execCommand("justifyCenter", false, null);
+			document.execCommand("styleWithCSS", false, "false");
+		}
 	};
 
 	return (
