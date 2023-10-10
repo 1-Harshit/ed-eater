155313b526bdf25139b12e35c399aee90a850cba
Harshit Raj <harshitr20@iitk.ac.in>
2023-10-10 20:36:04 +0530

    Fix Build

--- src/views/editor/marketplace/components/TableTopCreators.tsx
+++ src/views/editor/marketplace/components/TableTopCreators.tsx
@@ -96,7 +96,7 @@ export default function TopCreatorTable(props: { tableData: any }) {
 			)
 		})
 	];
-	const [ data, setData ] = React.useState(() => [ ...defaultData ]);
+	const data = defaultData;
 	const table = useReactTable({
 		data,
 		columns,
